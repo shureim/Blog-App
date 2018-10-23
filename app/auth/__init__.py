bdd2b2b46d7510b68708e654e3eeb9e38f146110
shureim <tarickaliabdi@gmail.com>
2018-10-23 08:01:26 +0300

    created a blueprint for auth

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,5 @@
-# from flask import Blueprint
-#
-# auth = Blueprint('auth',__name__)
-#
-# from . import views,forms
+from flask import Blueprint
+
+auth = Blueprint('auth',__name__)
+
+from . import views,forms
