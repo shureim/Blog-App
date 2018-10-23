1f5929478b5f1e5977a0ae3b2adc45597f993fa2
shureim <tarickaliabdi@gmail.com>
2018-10-23 08:16:10 +0300

    created the subscriberform class which also validates the email in the main.views.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -13,14 +13,14 @@ class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
 
-# class SubscriberForm(FlaskForm):
-#     email = StringField("Email Address",validators=[Required(),Email()])
-#     submit = SubmitField("Subscribe")
-#
-#     def validate_email(self,data_field):
-#         if Subscriber.query.filter_by(email =data_field.data).first():
-#             raise ValidationError("Account already subscribed with that email")
-#
+class SubscriberForm(FlaskForm):
+    email = StringField("Email Address",validators=[Required(),Email()])
+    submit = SubmitField("Subscribe")
+
+    def validate_email(self,data_field):
+        if Subscriber.query.filter_by(email =data_field.data).first():
+            raise ValidationError("Account already subscribed with that email")
+
 # class CommentForm(FlaskForm):
 #     comment = TextAreaField("Leave a Comment")
 #     submit = SubmitField('Submit')
