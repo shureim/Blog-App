c8ae93bb5b9523a48bcdb28595877c8675f338ff
shureim <tarickaliabdi@gmail.com>
2018-10-23 08:19:23 +0300

    created verify password for the users in the models.py

--- app/models.py
+++ app/models.py
@@ -23,20 +23,20 @@ class User(UserMixin,db.Model):
 
 
 
-#     @property
-#     def password(self):
-#         raise AttributeError('You cannot read the password attribute')
-#
-#     @password.setter
-#     def password(self, password):
-#         self.pass_secure = generate_password_hash(password)
-#
-#
-#     def verify_password(self,password):
-#         return check_password_hash(self.pass_secure,password)
-#
-#     def __repr__(self):
-#         return f'User {self.username}'
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
 #
 # class Subscriber(db.Model):
 #     __tablename__='subscribers'
