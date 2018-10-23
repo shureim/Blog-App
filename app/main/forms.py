68fc92f27f2e7c74eb1cba711a26fda8978cd3e2
shureim <tarickaliabdi@gmail.com>
2018-10-23 08:16:51 +0300

    created the commentform class in the main.views.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -21,6 +21,6 @@ class SubscriberForm(FlaskForm):
         if Subscriber.query.filter_by(email =data_field.data).first():
             raise ValidationError("Account already subscribed with that email")
 
-# class CommentForm(FlaskForm):
-#     comment = TextAreaField("Leave a Comment")
-#     submit = SubmitField('Submit')
+class CommentForm(FlaskForm):
+    comment = TextAreaField("Leave a Comment")
+    submit = SubmitField('Submit')
