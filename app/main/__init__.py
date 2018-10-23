b5dedffae8ba1a70d2a7f9c4f01b1408de69610d
shureim <tarickaliabdi@gmail.com>
2018-10-23 08:12:39 +0300

    created a blueprint for the main

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,3 @@
-# from flask import Blueprint
-# main = Blueprint('main',__name__)
-# from . import views,errors,forms
+from flask import Blueprint
+main = Blueprint('main',__name__)
+from . import views,errors,forms
