06e2effacd8946ef17b3522b70ae3f16d0d9f420
shureim <tarickaliabdi@gmail.com>
2018-10-23 08:28:26 +0300

    final changes

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -1,23 +1,23 @@
-# from app.models import Blog,User
-# from app import db
-#
-# def setUp(self):
-#
-#     self.user_tarek = User(username = 'tarek',password = 'tarek001', email = 'tarickaliabdi@gmail.com')
-#     self.new_blog = Blog(title='Civilized country',blog_content="Love and Peace is the key for a success country",user = self.user_James )
-#
-# def tearDown(self):
-#
-#     Blog.query.delete()
-#     User.query.delete()
-#
-# def test_check_instance_variables(self):
-#
-#     self.assertEquals(self.new_blog.title,'Civilized country')
-#     self.assertEquals(self.new_blog.blog_content,"Love and Peace is the key for a success country")
-#     self.assertEquals(self.new_blog.user,self.user_tarek)
-#
-# def test_save_blog(self):
-#
-#     self.new_blog.save_blog()
-#     self.assertTrue(len(Blog.query.all())>0)
+from app.models import Blog,User
+from app import db
+
+def setUp(self):
+
+    self.user_tarek = User(username = 'tarek',password = 'tarek001', email = 'tarickaliabdi@gmail.com')
+    self.new_blog = Blog(title='Civilized country',blog_content="Love and Peace is the key for a success country",user = self.user_James )
+
+def tearDown(self):
+
+    Blog.query.delete()
+    User.query.delete()
+
+def test_check_instance_variables(self):
+
+    self.assertEquals(self.new_blog.title,'Civilized country')
+    self.assertEquals(self.new_blog.blog_content,"Love and Peace is the key for a success country")
+    self.assertEquals(self.new_blog.user,self.user_tarek)
+
+def test_save_blog(self):
+
+    self.new_blog.save_blog()
+    self.assertTrue(len(Blog.query.all())>0)
