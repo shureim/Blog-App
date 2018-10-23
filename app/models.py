f790c7e20137f7b6047da6924bf738cb50898a0a
shureim <tarickaliabdi@gmail.com>
2018-10-23 08:18:29 +0300

    created the users table in the models.py

--- app/models.py
+++ app/models.py
@@ -1,28 +1,28 @@
-# from werkzeug.security import generate_password_hash,check_password_hash
-# from . import db, login_manager
-# from flask_login import UserMixin
-# from datetime import datetime
-#
-#
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
-#
-#
-# class User(UserMixin,db.Model):
-#     __tablename__ = 'users'
-#
-#     id = db.Column(db.Integer,primary_key = True)
-#     username = db.Column(db.String(255),index = True)
-#     email = db.Column(db.String(255),unique = True,index = True)
-#     bio = db.Column(db.String(255))
-#     profile_pic_path = db.Column(db.String())
-#     pass_secure = db.Column(db.String(255))
-#     role_id = db.Column(db.Integer,db.ForeignKey("roles.id"))
-#
-#
-#
-#
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import db, login_manager
+from flask_login import UserMixin
+from datetime import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey("roles.id"))
+
+
+
+
 #     @property
 #     def password(self):
 #         raise AttributeError('You cannot read the password attribute')
